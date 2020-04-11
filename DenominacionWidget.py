845132d09e3dcaef05e56b74d544c280a97c333b
Laura Santacruz <smlaura97@gmail.com>
2020-04-11 03:27:51 -0500

    Se cambio la vista de la prueba a widget

---
+++ DenominacionWidget.py
@@ -0,0 +1,139 @@
+#Vista de la prueba de Fluidez Verbal
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class FluidezVerbalWindowWidget(object):
+    def __init__(self, Form):
+        self.setupUi(Form)
+
+    def setupUi(self, Form):
+        """
+         Método empleado para especificar el contenido de la Interfáz gráfica, es generado por pyuic5.
+         Args:
+          Form: Ventana en la que se deplegará la interfáz gráfica (es un tipo de dato QtWidget.QWidget) 
+        """
+        Form.setObjectName("Form")
+        Form.resize(800, 598)
+        self.verticalLayoutWidget = QtWidgets.QWidget(Form)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(220, 0, 581, 541))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lblLogo = QtWidgets.QLabel(self.verticalLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lblLogo.sizePolicy().hasHeightForWidth())
+        self.lblLogo.setSizePolicy(sizePolicy)
+        self.lblLogo.setLineWidth(1)
+        self.lblLogo.setAlignment(QtCore.Qt.AlignCenter)
+        self.lblLogo.setObjectName("lblLogo")
+        self.horizontalLayout.addWidget(self.lblLogo)
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_4.setToolTip("")
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout_2.addWidget(self.label_4)
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setFormAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.formLayout.setContentsMargins(7, -1, 7, -1)
+        self.formLayout.setObjectName("formLayout")
+
+        self.label_8 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_8.setObjectName("label_8")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_8)
+        self.sbDenomImg = QtWidgets.QSpinBox(self.verticalLayoutWidget)
+        self.sbDenomImg.setObjectName("sbDenomImg")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.sbDenomImg)
+
+        self.label_9 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_9.setObjectName("label_9")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_9)
+        self.sbDenomImgT = QtWidgets.QSpinBox(self.verticalLayoutWidget)
+        self.sbDenomImgT.setObjectName("sbDenomImgT")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.sbDenomImgT)
+
+        self.label_10 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_10.setObjectName("label_10")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_10)
+        self.sVerbalComplejo = QtWidgets.QSpinBox(self.verticalLayoutWidget)
+        self.sVerbalComplejo.setObjectName("sVerbalComplejo")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.sVerbalComplejo)
+
+        self.label_11 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_11.setObjectName("label_11")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_11)
+        self.sbVerbalComplejoT = QtWidgets.QSpinBox(self.verticalLayoutWidget)
+        self.sbVerbalComplejoT.setObjectName("sbVerbalComplejoT")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.sbVerbalComplejoT)
+
+        self.label_12 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_12.setObjectName("label_12")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.label_12)
+        self.sbSemejanzaAbs = QtWidgets.QSpinBox(self.verticalLayoutWidget)
+        self.sbSemejanzaAbs.setObjectName("sbSemejanzaAbs")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.sbSemejanzaAbs)
+
+        self.verticalLayout_2.addLayout(self.formLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.pbStart = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pbStart.sizePolicy().hasHeightForWidth())
+        self.pbStart.setSizePolicy(sizePolicy)
+        self.pbStart.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.pbStart.setObjectName("pbStart")
+        self.horizontalLayout_2.addWidget(self.pbStart)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout_2.setStretch(0, 1)
+        self.verticalLayout_2.setStretch(1, 5)
+        self.verticalLayout.addLayout(self.verticalLayout_2)
+        self.verticalLayout.setStretch(0, 1)
+        self.verticalLayout.setStretch(1, 6)
+        self.lWVistas = QtWidgets.QListWidget(Form)
+        self.lWVistas.setGeometry(QtCore.QRect(0, 90, 221, 451))
+        self.lWVistas.setObjectName("lWVistas")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        """
+         Método empleado paraasignar el contenido de la Interfáz gráfica, es generado por pyuic5.
+         Args:
+          Form: Ventana en la que se deplegará la interfáz gráfica (es un tipo de dato QtWidget.QWidget) 
+        """
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.lblLogo.setText(_translate("Form", "Logo"))
+        self.label.setText(_translate("Form", "SYNAPPS"))
+        self.label_4.setText(_translate("Form", "Ingrese los datos del paciente"))
+        self.label_8.setText(_translate("Form", "Denominacion Imágenes: "))
+        self.label_9.setText(_translate("Form", "Denon imágenes T:"))
+        self.label_10.setText(_translate("Form", "Material Verbal Complejo:"))
+        self.label_11.setText(_translate("Form", "Material Verbal Complejo T"))
+        self.label_12.setText(_translate("Form", "Semejanza Abstracción:"))
+        self.pbStart.setText(_translate("Form", "Registrar Prueba"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = FluidezVerbalWindowWidget(Form)
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
