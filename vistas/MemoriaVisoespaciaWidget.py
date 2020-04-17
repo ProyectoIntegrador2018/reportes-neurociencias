16bfab5399e9f7ade2e77626d3aa7d6c4ecbdb15
Laura Santacruz <smlaura97@gmail.com>
2020-04-17 01:33:34 -0500

    Agregar comentarios

--- vistas/MemoriaVisoespaciaWidget.py
+++ vistas/MemoriaVisoespaciaWidget.py
@@ -105,13 +105,13 @@ class MemoriaVisoespaciaWidget(object):
         self.pbStart.setText(_translate("Form", "Registrar Prueba"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = MemoriaVisoespaciaWidget(Form)
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec_())
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     Form = QtWidgets.QWidget()
+#     ui = MemoriaVisoespaciaWidget(Form)
+#     ui.setupUi(Form)
+#     Form.show()
+#     sys.exit(app.exec_())
 
 
