f09dc72ddebedce52bd8791133878c4e4ca2a109
Fernando Romero <ferdav19@gmail.com>
2020-04-28 18:50:43 -0500

    Feature/abstraccion (#16)
    * Commit forzoso
    
    * Se modificaron rangos máximos que pueda tener la prueba de fluidezVerbal
    
    * Se codificó la prueba de tmt
    
    * Se realizaron el mvc de la prueba TMT, así como las pruebas de esta
    
    * Se añadió el baremo y mvc de la prueba Abstracción, se añadió un método que reemplaza parte del código que se repetía en cada función que cambiaba la vista en el master controller

---
+++ controladores/AbstraccionController.py
@@ -0,0 +1,84 @@
+#Controlador de la vista de AbstraccionWindow
+from PyQt5 import QtWidgets, QtCore
+from vistas.AbstraccionWindowWidget import *
+from MainWindowController import *
+from ReporteModel import *
+from pruebas.AbstraccionPrueba import *
+from PruebaModel import *
+from ControllerModel import *
+
+
+class AbstraccionController(QtWidgets.QWidget, ControllerModel):
+	#Atributo empleado para realizar el cambio de vista
+	switch_window = QtCore.pyqtSignal(object, object)
+
+	def __init__(self, mainWindow, reporteModel=None):
+		QtWidgets.QWidget.__init__(self)
+		self.abstraccionView = AbstraccionWindowWidget(mainWindow)
+		self.abstraccionView.pbStart.clicked.connect(self.getDatos)
+		self.reporteModel = reporteModel
+		self.invalidArgs = list()
+	
+	def changeView(self):
+		"""
+		 Método encargado de notificar los elementos que serán pasados como parámetros a la siguiente vista
+		"""
+		self.switch_window.emit(self.invalidArgs, self.abstraccionPrueba)
+
+
+	def getDatos(self):
+		"""
+		 Método que toma los datos ingresados en la vista de TMT
+		"""
+		view = self.abstraccionView
+		valSemAbs = view.sbAbstraccion.value()
+
+		valores = (valSemAbs)
+		
+		self.abstraccionPrueba = AbstraccionPrueba(valores)
+		
+		datos = [self.reporteModel.reporte['edad']]
+		
+		if (valSemAbs < 0 or valSemAbs > 12):
+			self.addInvalidArg("TMT A")
+
+		if len(self.invalidArgs) == 0:
+			self.abstraccionPrueba.calcularPERP(datos)
+			
+		self.changeView()
+
+	def emptyInvalidArgs(self):
+		"""
+		 Método que se encarga de vacíar la lista de elementos inválidos en la vista
+		"""
+		self.invalidArgs = list()
+
+	def addInvalidArg(self, arg):
+		"""
+		 Método que se encarga de añadir a la lista de elementos inválidos, aquel parámetro especificado
+		 Args:
+		  arg: String a añadir a la lista de elementos inválidos
+		"""
+		
+		if len(self.invalidArgs) == 0:
+			self.invalidArgs = [arg]
+		else:
+			tempList = self.invalidArgs
+			tempList.append(arg)
+			self.invalidArgs = tempList
+
+	def getListMenu(self):
+		"""
+		 Método que se regresa el id del menu en la vista de TMT
+		"""
+		return self.abstraccionView.lWVistas
+
+
+# Pruebas unitarias
+#if __name__ == "__main__":
+#    import sys
+#    app = QtWidgets.QApplication(sys.argv)
+#    fluidezWindow = QtWidgets.QWidget()
+#    fluidezVerbalController = TMTController(fluidezWindow)
+#    fluidezWindow.show()
+#    sys.exit(app.exec_())
