23437baa4cbeb9d1fe42d9a6456319d1e464c020
Martin Ruiz <martin.ruiz.mares@gmail.com>
2021-04-27 18:42:25 -0500

    widgets refactor

--- src/main/python/vistas/AbstraccionWindowWidget.py
+++ src/main/python/vistas/AbstraccionWindowWidget.py
@@ -1,8 +1,6 @@
 # Vista de la prueba de Abstraccion
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QWidget
-from PyQt5.QtGui import QPixmap, QIcon
-from AppCtxt import APPCTXT
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QLabel, QWidget
 from .mixins import WindowWidgetMixin
 
 class AbstraccionWindowWidget(WindowWidgetMixin):
@@ -13,52 +11,16 @@ class AbstraccionWindowWidget(WindowWidgetMixin):
           Form: Ventana en la que se deplegará la interfáz gráfica (es un tipo de dato QtWidget.QWidget) 
         """
         super().setupUI(Form)
-        Form.setObjectName("Form")
-        Form.resize(800, 598)
-        self.verticalLayoutWidget = QtWidgets.QWidget(Form)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(220, 0, 581, 541))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
 
-        self.lblLogo = QtWidgets.QLabel(self.verticalLayoutWidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lblLogo.sizePolicy().hasHeightForWidth())
-        self.lblLogo.setSizePolicy(sizePolicy)
-        self.lblLogo.setMaximumSize(QtCore.QSize(1697/3.5, 312/3.5))
-        self.lblLogo.setLineWidth(1)
-        self.lblLogo.setText("")
-        self.lblLogo.setPixmap(QtGui.QPixmap(APPCTXT().get_resource("logo3.png")))
-        self.lblLogo.setScaledContents(True)
-        self.lblLogo.setAlignment(QtCore.Qt.AlignCenter)
-        self.lblLogo.setObjectName("lblLogo")
-        self.horizontalLayout.addWidget(self.lblLogo)
-
-        # self.lblLogo.show()
-        # self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
-        # self.label.setAlignment(QtCore.Qt.AlignCenter)
-        # self.label.setObjectName("label")
-        # self.horizontalLayout.addWidget(self.label)
         self.horizontalLayout.setStretch(0,1)
         self.horizontalLayout.setStretch(1,3)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
         self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label_4.setToolTip("")
         self.label_4.setAlignment(QtCore.Qt.AlignCenter)
         self.label_4.setObjectName("label_4")
         self.verticalLayout_2.addWidget(self.label_4)
-        self.formLayout = QtWidgets.QFormLayout()
-        self.formLayout.setFormAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
-        self.formLayout.setContentsMargins(7, -1, 7, -1)
-        self.formLayout.setObjectName("formLayout")
-        self.formLayout.setFieldGrowthPolicy(QtWidgets.QFormLayout.ExpandingFieldsGrow)
+
         self.label_8 = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label_8.setObjectName("label_8")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_8)
@@ -67,37 +29,9 @@ class AbstraccionWindowWidget(WindowWidgetMixin):
         self.sbAbstraccion.setRange(0,12)
         self.sbAbstraccion.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.sbAbstraccion)
-        self.verticalLayout_2.addLayout(self.formLayout)
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_2.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.pbStart = QtWidgets.QPushButton(self.verticalLayoutWidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pbStart.sizePolicy().hasHeightForWidth())
-        self.pbStart.setSizePolicy(sizePolicy)
-        self.pbStart.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.pbStart.setObjectName("pbStart")
-        self.horizontalLayout_2.addWidget(self.pbStart)
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
-        self.verticalLayout_2.setStretch(0, 1)
-        self.verticalLayout_2.setStretch(1, 5)
-        self.verticalLayout.addLayout(self.verticalLayout_2)
-        self.verticalLayout.setStretch(0, 1)
-        self.verticalLayout.setStretch(0, 0)
-        self.lWVistas = QtWidgets.QListWidget(Form)
-        self.lWVistas.setGeometry(QtCore.QRect(0, 90, 221, 451))
-        self.lWVistas.setObjectName("lWVistas")
-
-        self.progressBar = QtWidgets.QProgressBar(Form)
-        self.progressBar.setGeometry(QtCore.QRect(127, 560, 601, 23))
-        self.progressBar.setProperty("value", 24)
-        self.progressBar.setObjectName("progressBar")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
-
     def retranslateUi(self, Form):
         """
          Método empleado paraasignar el contenido de la Interfáz gráfica, es generado por pyuic5.
@@ -106,20 +40,8 @@ class AbstraccionWindowWidget(WindowWidgetMixin):
         """
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Prueba Abstracción"))
-        # self.lblLogo.setText(_translate("Form", "Logo"))
-        
-        # self.label.setText(_translate("Form", "SYNAPPS"))
+
         self.label_4.setText(_translate("Form", "Ingrese los datos de la prueba Abstracción"))
         self.label_8.setText(_translate("Form", "Puntuación en semejanza-abstracción: "))
         self.pbStart.setText(_translate("Form", "Registrar Prueba"))
         self.backButton.setText(_translate("Form", "Regresar al Menu"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = AbstraccionWindowWidget(Form)
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec_())
