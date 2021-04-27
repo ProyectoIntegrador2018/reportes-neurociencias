f06cac6b749781ac74e1ac8aeccd796082c57125
Pablo Andrade <pabloemilioandrade@gmail.com>
2021-04-27 18:15:23 -0400

    Refactor Controllers

--- src/main/python/controladores/DenominacionController.py
+++ src/main/python/controladores/DenominacionController.py
@@ -1,82 +1,29 @@
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtCore
 from vistas.DenominacionWidget import DenominacionWidget
-# from MainWindowController import *
-# from ReporteModel import *
 from pruebas.DenominacionPrueba import DenominacionPrueba
-# from PruebaModel import *
-from ControllerModel import * 
+from .mixins import WindowControllerMixin
 
-class DenominacionController(QtWidgets.QWidget, ControllerModel):
+class DenominacionController(WindowControllerMixin):
     # Atributo empleado para realizar el cambio de vista
     switch_window = QtCore.pyqtSignal(object, object, bool)
+    
 
-    def __init__(self, mainWindow, reporteModel=None):
-        QtWidgets.QWidget.__init__(self)
-        self.denominacionView = DenominacionWidget(mainWindow)
-        self.denominacionView.pbStart.clicked.connect(self.getDatos)
-        self.denominacionView.backButton.clicked.connect(self.returnView)
-        self.reporteModel = reporteModel
-        self.invalidArgs = list()
-    
-    def returnView(self):
-        """
-		 Método encargado de notificar los elementos que serán pasados como parámetros a la siguiente vista
-		"""
-        self.switch_window.emit(self.invalidArgs, self.denominacionPrueba, True)
-	
-    def changeView(self):
-        """
-        Metodo de notificar los elementos que seran pasados a la siguiene vista como parametros
-        """
-        # print("Ando en changeView de Denominacion")
-        self.switch_window.emit(self.invalidArgs, self.denominacionPrueba, False)
-    
+    def getWidgetClass(self):
+	    return DenominacionWidget
+
     def getDatos(self):
         """
         Metodo para tomar los datos ingresados en la prueba de denominacion
         """
-        view = self.denominacionView
+        view = self.view
         denomimgs = view.sbDenomImg.value()
         # print("Valor denominacion imagenes: ", denomimgs)
         denomimgT = view.sbDenomImgT.value()
         # print("Valor denominacion imagenes T: ", denomimgT)
 
-        valores = (denomimgs, denomimgT) 
+        valores = (denomimgs, denomimgT)
 
-        self.denominacionPrueba = DenominacionPrueba(valores)
+        self.test = DenominacionPrueba(valores)
 
-        self.denominacionPrueba.calcularPERP()
-
-
+        self.test.calcularPERP()
         self.changeView()
-
-    
-    def getListMenu(self):
-        """
-        Regresa el id del meni en la vista Denominacion
-        """
-        return self.denominacionView.lWVistas
-
-    def getProgressBar(self):
-        """
-         Método que se encarga de regresar el valor de la barra de progreso
-        """
-        return self.denominacionView.progressBar
-
-    def updateButtonText(self, text):
-        """
-         Método que se encarga de actulaizar el texto del botón de la vista
-         Args:
-          text: Objeto de tipo str que contiene el nuevo valor a asignar al botón presente en las pruebas
-        """
-        self.denominacionView.pbStart.setText(text)
-
-
-# Pruebas unitarias
-#if __name__ == "__main__":
-#    import sys
-#    app = QtWidgets.QApplication(sys.argv)
-#    denominacionWindow = QtWidgets.QWidget()
-#    denominacionController = DenominacionController(denominacionWindow)
-#    denominacionWindow.show()
-#    sys.exit(app.exec_())
--- src/main/python/controladores/HopkinsController.py
+++ src/main/python/controladores/HopkinsController.py
@@ -1,101 +1,29 @@
-#Controlador de la vista de HopkinsWindowWidget
-from PyQt5 import QtWidgets, QtCore
-from vistas.HopkinsWindowWidget import *
-from MainWindowController import *
-from ReporteModel import *
-from pruebas.HopkinsPrueba import *
-from PruebaModel import *
-from ControllerModel import *
+from PyQt5 import QtCore
+from vistas.HopkinsWindowWidget import HopkinsWindowWidget
+from pruebas.HopkinsPrueba import HopkinsPrueba
+from .mixins import WindowControllerMixin
 
-
-
-class HopkinsController(QtWidgets.QWidget, ControllerModel):
+class HopkinsController(WindowControllerMixin):
 	#Atributo empleado para realizar el cambio de vista
 	switch_window = QtCore.pyqtSignal(object, object, bool)
 
-	def __init__(self, mainWindow, reporteModel=None):
-		QtWidgets.QWidget.__init__(self)
-		self.hopkinsView = HopkinsWindowWidget(mainWindow)
-		self.hopkinsView.pbStart.clicked.connect(self.getDatos)
-		self.hopkinsView.backButton.clicked.connect(self.returnView)
-		self.reporteModel = reporteModel
-		self.invalidArgs = list()
-	
-	def returnView(self):
-		"""
-		 Método encargado de notificar los elementos que serán pasados como parámetros a la siguiente vista
-		"""
-		self.switch_window.emit(self.invalidArgs, self.hopkinsPrueba, True)
-	
-	def changeView(self):
-		"""
-		 Método encargado de notificar los elementos que serán pasados como parámetros a la siguiente vista
-		"""
-		self.switch_window.emit(self.invalidArgs, self.hopkinsPrueba, False)
+	def getWidgetClass(self):
+		return HopkinsWindowWidget
 
 	def getDatos(self):
 		"""
 		 Método que toma los datos ingresados en la vista de Hopkins
 		"""
-		view = self.hopkinsView
+		view = self.view
 		total_recall = view.sbSpan.value()
 		delayed_recall = view.sbTotal.value()
 
 		valores = [total_recall, delayed_recall]
-		self.hopkinsPrueba = HopkinsPrueba(valores)
+		self.test = HopkinsPrueba(valores)
 
 		#toma anos de escolaridad del paciente
 		datos = self.reporteModel.reporte['edad']
 		
-		self.hopkinsPrueba.calcularPERP(datos)
+		self.test.calcularPERP(datos)
 			
 		self.changeView()
-
-	def emptyInvalidArgs(self):
-		"""
-		 Método que se encarga de vacíar la lista de elementos inválidos en la vista
-		"""
-		self.invalidArgs = list()
-
-	def addInvalidArg(self, arg):
-		"""
-		 Método que se encarga de añadir a la lista de elementos inválidos, aquel parámetro especificado
-		 Args:
-		  arg: String a añadir a la lista de elementos inválidos
-		"""
-		
-		if len(self.invalidArgs) == 0:
-			self.invalidArgs = [arg]
-		else:
-			tempList = self.invalidArgs
-			tempList.append(arg)
-			self.invalidArgs = tempList
-
-	def getListMenu(self):
-		"""
-		 Método que se regresa el id del menu en la vista de Hopkins
-		"""
-		return self.hopkinsView.lWVistas
-
-
-	def getProgressBar(self):
-		"""
-		 Método que se encarga de regresar el valor de la barra de progreso
-		"""
-		return self.hopkinsView.progressBar
-
-	def updateButtonText(self, text):
-		"""
-		 Método que se encarga de actulaizar el texto del botón de la vista
-		"""
-		self.hopkinsView.pbStart.setText(text)
-
-
-# Pruebas unitarias
-#if __name__ == "__main__":
-#	import sys
-#	app = QtWidgets.QApplication(sys.argv)
-#	fluidezWindow = QtWidgets.QWidget()
-#	fluidezVerbalController = HopkinsController(fluidezWindow)
-#	fluidezWindow.show()
-#	sys.exit(app.exec_())
